7e1b96580e81a3e05c07a935721b0f0d97a001a0
keanu159 <kiritokun852@outlook.com>
2019-08-14 10:16:03 -0300

    creada a classe people

--- oo/people.py
+++ oo/people.py
@@ -1,2 +1,10 @@
 class People:
-    pass
+    def cumprimentar (self):
+        return f'ola{id(self)}'
+
+
+if __name__ == '__main__':
+    p = People()
+    print(People.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
